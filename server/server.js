23e9a75cdc4dfb38f32f1df9bf7340c5f0c38b9e
abhinavtripathy <abhinavtripathy99@gmail.com>
2020-11-03 18:54:54 -0500

    initial endpoints

--- server/server.js
+++ server/server.js
@@ -3,11 +3,60 @@ const app = express()
 const port = 8080
 
 app.use(express.static('public/'));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// POST Patients 
+app.post('/patients', (req, res) => {
+    console.log(req.body);
+    res.send("done");
+});
+
+// GET Patients 
+app.get('/patients/:id', (req, res) => {
+    console.log(req.params.id);
+    res.send("done");
+});
+
+// PUT Patients 
+app.put('/patients/:id', (req, res) => {
+    console.log(req.params.id);
+    res.send("done");
+});
+
+// DELETE Patients 
+app.delete('/patients/:id', (req, res) => {
+    console.log(req.params.id);
+    res.send("done");
+});
+
+
+
+// POST Drivers
+app.post('/drivers', (req, res) => {
+    console.log(req.body);
+    res.send("done");
+});
+
+// GET Patients 
+app.get('/drivers/:id', (req, res) => {
+    console.log(req.params.id);
+    res.send("done");
+});
+
+// PUT Patients 
+app.put('/drivers/:id', (req, res) => {
+    console.log(req.params.id);
+    res.send("done");
+});
+
+// DELETE Patients 
+app.delete('/drivers/:id', (req, res) => {
+    console.log(req.params.id);
+    res.send("done");
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-})
+});
