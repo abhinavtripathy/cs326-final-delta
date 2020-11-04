710493de23d95414719a5763d11ff2af4c9b5451
abhinavtripathy <abhinavtripathy99@gmail.com>
2020-11-03 20:14:15 -0500

    fake data generation

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const faker = require('faker');
+const { fake } = require('faker');
 const app = express()
 const port = 8080
 
 app.use(express.static('public/'));
 app.use(express.json());
+
+let database = {};
+database['patients'] = [];
+database['drivers'] = [];
+database['hospitals'] = [];
 
 // POST Patients 
 app.post('/patients', (req, res) => {
@@ -57,6 +64,69 @@ app.delete('/drivers/:id', (req, res) => {
 
 
 
+// PUT Patients 
+app.put('/hospitals/:id', (req, res) => {
+    console.log(req.params.id);
+    res.send("done");
+});
+
+
+
+
+
+
+function generateFakeData() {
+
+    for (let i = 0; i < 10; i++) {
+
+        let patientjson = {
+            first_name: faker.name.firstName(),
+            last: faker.name.lastName(),
+            age: Math.floor(Math.random() * (99) + 1),
+            phone: faker.phone.phoneNumber("#########"),
+            emergency: faker.phone.phoneNumber("#########"),
+            email: faker.internet.email(),
+            address: faker.address.streetAddress(),
+            pickup: "Door C",
+            driver: Math.floor(Math.random() * (1000000000000000000000) + 1),
+            status: "waiting"
+        }
+
+        database['patients'].push(patientjson);
+
+        let driverjson = {
+            first_name: faker.name.firstName(),
+            last: faker.name.lastName(),
+            age: Math.floor(Math.random() * (99) + 1),
+            phone: faker.phone.phoneNumber("#########"),
+            email: faker.internet.email(),
+            car: {
+                make: faker.vehicle.manufacturer(),
+                model: faker.vehicle.model(),
+                color: faker.vehicle.color(),
+                plate: faker.vehicle.vin()
+            },
+            patients: [Math.floor(Math.random() * (1000000000000000000000) + 1), Math.floor(Math.random() * (1000000000000000000000) + 1)],
+            verified: Math.random() >= 0.5
+        }
+
+        database['drivers'].push(driverjson);
+        
+        let hospitaljson = {
+            name: faker.address.city() + " Medical Center"
+
+        }
+
+        database['hospitals'].push(hospitaljson);
+
+    }
+
+    console.log(database);
+
+}
+
+generateFakeData();
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
