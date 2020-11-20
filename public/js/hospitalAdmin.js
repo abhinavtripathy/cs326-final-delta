61c7f0884ef61706dd576a8e59cc94fa2e059472
Aditya Narayanan <aditya.narayanan26@gmail.com>
2020-11-20 15:39:54 -0800

    Fix hospital admin (#76)
    * change variable
    
    * added value instead of innerText
    
    * added space between FN and LN

--- public/js/hospitalAdmin.js
+++ public/js/hospitalAdmin.js
@@ -29,7 +29,8 @@ function addDriver(driverProfile) {
     const driver = document.createElement('input');
     driver.type = "text";
     driver.className = "form-control";
-    driver.value = driverProfile.first_name + driverProfile.last_name;
+
+    driver.value = driverProfile.first_name + " " + driverProfile.last_name;
 
     inputGroup.appendChild(prepend);
     inputGroup.appendChild(driver);
