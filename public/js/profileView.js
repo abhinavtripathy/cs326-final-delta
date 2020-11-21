296d7aaadf69e53c8b335578e86d54c6d67e834e
Aditya Narayanan <aditya.narayanan26@gmail.com>
2020-11-21 13:37:51 -0800

    added profileView js file

---
+++ public/js/profileView.js
@@ -0,0 +1,4 @@
+/*
+    * If patient is viewing profile, then do a fetch with ID to the patient DB and generate html to add volunteer driver to the existing profile view
+    * If driver is viewing profile, add details like car details to existing data.
+*/
