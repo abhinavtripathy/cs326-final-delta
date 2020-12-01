4675b57b233112b31975a6f2eb245f64385b1dd9
abhinavtripathy <abhinavtripathy99@gmail.com>
2020-12-01 05:19:17 -0500

    minor

--- server/server.js
+++ server/server.js
@@ -82,10 +82,10 @@ const getSaltHashOf = async (email, isPatient) => {
 const strategy = new LocalStrat({usernameField: 'email', passwordField: 'password'},
     async (email, pass, done) => {
         if (await getSaltHashOf(email, true) === undefined && await getSaltHashOf(email, false) === undefined) {
-	    return done(null, false, {'message' : 'No user with that email exists'});
+	        return done(null, false, {'message' : 'No user with that email exists'});
         }
         if (!(await checkPass(email, pass))) {
-	    await new Promise((r) => setTimeout(r, 2000)); // This does not stop parallel requests from being sent. A more secure method might be an account-wide retry counter but this implementation was not covered in the scope of the class
+	        await new Promise((r) => setTimeout(r, 2000)); // This does not stop parallel requests from being sent. A more secure method might be an account-wide retry counter but this implementation was not covered in the scope of the class
 	        return done(null, false, { 'message' : 'Incorrect password' });
         }
         return done(null, email);
@@ -95,7 +95,7 @@ const mustBeAuthenticated = async(req, res, next) => req.isAuthenticated() ? nex
 
 const mustBeDriver = [mustBeAuthenticated, async (req, res, next) => !(await userInfo(req.user)).isPatient ? next() : res.redirect('/mustBeDriver.html')];
 
-const mustBePatient = [mustBeAuthenticated, async (req, res, next) => await userInfo(req.user).isPatient ? next() : res.redirect('/mustBePatient.html')];
+// const mustBePatient = [mustBeAuthenticated, async (req, res, next) => await userInfo(req.user).isPatient ? next() : res.redirect('/mustBePatient.html')];
 
 const userExists = async email => await getSaltHashOf(email, false) !== undefined || await getSaltHashOf(email, true) !== undefined;
 
